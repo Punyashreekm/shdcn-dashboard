af6ed23dc3f7c303c68daae0ba2e416bc5f1e2e5
punya <punya8144@gmail.com>
2025-09-27 20:23:50 +0530

    navbar, dropdown menu, SR only class name, aschild in radix UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,7 @@ import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 
 const Homepage = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="xl" className="rounded-full">
-        <CirclePlus />
-        Click me
-      </Button>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default Homepage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 flex justify-between items-center ">
+      {/* LEFT */}
+      collapseButton
+      {/* RIGHT */}
+      <div className=" flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="w-[1.2rem] h-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="w-[1.2rem] h-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="w-[1.2rem] h-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="outline" size="icon">
+              <SquareMenu />
+              <span className="sr-only">Open Menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
